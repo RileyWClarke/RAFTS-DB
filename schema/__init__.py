486b818dc7be6a3e3869f251eb0721be317c036f
Riley Clarke <rclarke1995@gmail.com>
2025-10-16 17:15:45 -0400

    Initial commit

---
+++ schema/__init__.py
@@ -0,0 +1,12 @@
+REFERENCE_TABLES = [
+    "Publications",
+    "Telescopes",
+    "Instruments",
+    "PhotometryFilters",
+    "Versions",
+    "RegimeList",
+    "AssociationList",
+    "SourceTypeList",
+    "ParameterList",
+    "CompanionList",
+]
